f373d72a2d01845eba1f42a48a9d69d15e26f0be
vicziani <viczian.istvan@gmail.com>
2019-07-23 21:15:06 +0200

    További Mockito példák

---
+++ README.md
@@ -0,0 +1,10 @@
+JTechLog Mockito tutorial
+=========================
+
+Ez a program a JTechLog (<http://jtechlog.hu>) blog "Mockito" cikkéhez készült példaprogram.
+Egy bevezetést nyújt a Mockito lehetőségeibe. Mavennel buildelhető, és a letöltést követően az
+'mvn jetty:test' paranccsal futtathatóak a teszt esetek.
+
+Felhasznált technológiák: JUnit, Mockito, Maven
+
+viczian.istvan a gmail-en
--- src/test/java/mockito/EmployeeServiceTest.java
+++ src/test/java/mockito/EmployeeServiceTest.java
@@ -2,11 +2,14 @@ package mockito;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,9 +23,28 @@ public class EmployeeServiceTest {
 
     @Test
     void testCreateEmployee() {
+        var messageSender = mock(MessageSender.class);
+        var employeeService = new EmployeeService(messageSender);
+
         employeeService.createEmployee("John Doe");
 
         verify(messageSender).sendMessage(any());
+    }
+
+    @Test
+    void testCreateEmployeeWithExtension() {
+        employeeService.createEmployee("John Doe");
+
+        verify(messageSender).sendMessage(any());
+    }
+
+    @Test
+    void testSendMessageParameter() {
+        ArgumentCaptor<Employee> employeeArgumentCaptor = ArgumentCaptor.forClass(Employee.class);
+        employeeService.createEmployee("John Doe");
+
+        verify(messageSender).sendMessage(employeeArgumentCaptor.capture());
+        assertEquals("John Doe", employeeArgumentCaptor.getValue().getName());
     }
 
 }
